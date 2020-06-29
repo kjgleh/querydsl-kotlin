6e017b9e9b59d866fae1a74de73c576b93929023
김정근 <gogoogjg@tripstore.kr>
2020-06-29 13:35:40 +0900

    querydsl 설정을 추가한다

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.3.72"
     kotlin("plugin.spring") version "1.3.72"
     kotlin("plugin.jpa") version "1.3.72"
+    kotlin("kapt") version "1.3.72"
 }
 
 group = "com.example"
@@ -17,11 +18,14 @@ repositories {
 }
 
 dependencies {
+    val queryDslVersion="4.3.1"
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.querydsl:querydsl-jpa:${queryDslVersion}")
+    kapt ("com.querydsl:querydsl-apt:${queryDslVersion}:jpa")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
---
+++ src/test/kotlin/com/example/querydslk/ReservationRepositoryTest.kt
@@ -0,0 +1,5 @@
+package com.example.querydslk
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ReservationRepositoryTest
