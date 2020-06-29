0af612fa26035292207a54ccd455484369e3ae5c
김정근 <gogoogjg@tripstore.kr>
2020-06-29 12:40:57 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/example/querydslk/QuerydslkApplication.kt
@@ -0,0 +1,11 @@
+package com.example.querydslk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuerydslkApplication
+
+fun main(args: Array<String>) {
+    runApplication<QuerydslkApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/querydslk/QuerydslkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.querydslk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuerydslkApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
